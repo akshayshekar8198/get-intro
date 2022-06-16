483b7a6f3b39d2a95e8a90be25e012cc2a8d0695
Akshay <akshays8198@gmail.com>
2022-06-16 16:47:01 +0530

    operator and loops

---
+++ js/loop.js
@@ -0,0 +1,24 @@
+var people = ['person1 ','person2','person3'];
+var number = [1,2,3,4,5,6,7,8,9,10];
+var mixed =[1,'person1',2,'person3','person4'];
+var data = [
+    { name: 'person1',rank:1},
+    { name: 'person2',rank:2},
+    { name: 'person3',rank:3},
+];
+//console.log(data);
+
+//for(var i = 0; i<data.length; i++) {
+//    console.log('typeof  data[i]',data[i]);
+//};
+
+ var i = 0;
+//while(i<data.length){
+  //  console.log(typeof data[i],data[i]);
+   // i++;
+//}
+do{
+    console.log(typeof data[i],data[i]);
+    i++;
+}
+while(i<data.length);
