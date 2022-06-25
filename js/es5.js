b2ade390181db35c5edca86cbeb2f9315fa9334d
Akshay <akshays8198@gmail.com>
2022-06-25 15:02:45 +0530

    25 june 2

--- js/es5.js
+++ js/es5.js
@@ -1,13 +1,3 @@
-var a = 1;
-var b = "1";
-
-
-// var c = b;
-// console.log(c)
-console.log(a == b);
-
-
-
 // var d = "akshay";
 // var e = d;
 // e = "anythinhgs";
@@ -17,10 +7,40 @@ console.log(a == b);
 var tv ={
     ison: false
 }
-console.log(JSON.stringify(TV));
-function turnon (device){
+console.log(JSON.stringify(tv));
+function turnOn (device){
     device.ison = true;
+    device = {
+        isOn: false
+    };
 
 }
-turnon (tv)
+turnOn (tv)
 console.log(tv)
+
+//new
+
+function sum  (a,b){
+    console.log
+    return a+b;
+    }
+console.log(sum(2,8))
+
+function sub  (a,b){
+    return a-b;
+}
+function task  (c,a,b){
+    return c( a,b );
+}
+//9845426946
+console.log(task (sum,5,6));
+
+var cars = ['volve','innova','bmw','bmw','bmw'];
+console.log(cars);
+function filtertask(item){
+   console.log(item);
+    return item === 'bmw' ;
+
+}
+
+console.log(cars.filter(filtertask));
