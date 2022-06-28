157aaa0a21f7bd5627bab09ec09a5b4e197f61d4
Akshay <akshays8198@gmail.com>
2022-06-28 16:28:39 +0530

    es61

---
+++ js/es6.js
@@ -0,0 +1,11 @@
+function person (fname){
+    this.firstname = fname;
+}
+ var personone = new person('akshay')
+ console.log( personone)
+ var persontwo = new person('akki')
+ console.log( persontwo);
+
+
+//for array format 
+
